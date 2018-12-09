c0803e844842ad23ba69882071c00d8fac59ec27
Andrew Mason <andrew@fixedpoint.xyz>
2018-12-09 06:07:31 -0800

    Fix indentation and solve day1 part 2

--- 01/main.py
+++ 01/main.py
@@ -2,22 +2,28 @@ import argparse
 
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument(
-		'nums',
-		metavar='N',
-		type=int,
-		nargs='+',
-	)
-	parser.add_argument(
-        '--sum',
-        dest='accumulate',
-        action='store_const',
-        const=sum,
-        default=sum,
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'nums',
+        metavar='N',
+        type=int,
+        nargs='+',
     )
-	args = parser.parse_args()
-	print(args.accumulate(args.nums))
+    args = parser.parse_args()
+    frequency = 0
+    first_repitition = None
+    frequencies = set([0])
+    while first_repitition is None:
+        for n in args.nums:
+            frequency += n
+            if frequency not in frequencies:
+                frequencies.add(frequency)
+            else:
+                if first_repitition is None:
+                    first_repitition = frequency
+
+    print(sum(args.nums))
+    print(first_repitition)
 
 
 if __name__ == '__main__':
